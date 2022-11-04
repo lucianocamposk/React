dc274356d83ad727dd7c0b412d9fff549df2d1c8
lucianocamposk <lucho.campos@outlook.com>
2022-11-03 22:04:50 -0300

    customHooks

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { CounterApp } from './01-useState/CounterApp'
+//import { CounterApp } from './01-useState/CounterApp'
+//import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 //import { HooksApp } from './HooksApp'
-import './index.css'
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+import { SimpleForm } from './02-useEffect/SimpleForm'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterApp />
+    <FormWithCustomHook />
   </React.StrictMode>
 )
